d0bab951105bd419a8e042991500bd21e38749a4
Sunny Yang <sunnyadn@foxmail.com>
2020-10-22 15:13:11 +0800

    Finish practises

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const zip = function(keys, values) {
+    const data = {};
+    for (let i = 0; i < keys.length; i++) {
+        data[keys[i]] = values[i];
+    }
+
+    return data;
+}
+
+const parseData = function(input) {
+    const keys = input.column.map(column => column.name);
+    const result = input.data.map(values => zip(keys, values));
+
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = function(array, insertions) {
+    const sortedInsertions = [...insertions];
+    sortedInsertions.sort((a, b) => b.index - a.index);
+
+    for (const insertion of sortedInsertions) {
+        array.splice(insertion.index, 0, insertion.content);
+    }
+
+    return array;
+}
+
 export { inject };
